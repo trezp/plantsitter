394be0fe84564e06b09bb302b21deec63df29b20
Treasure Porth <treasure.rose@gmail.com>
2019-06-21 00:40:55 -0700

    added meters

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-alert("stuff")
+const plants = [...document.querySelectorAll('.plant-card')]; 
+
+console.log(plants)
+
+const date = new Date(); 
+const today = date.getDate();
+console.log(today)
+const dateLastWatered = 0;
+
